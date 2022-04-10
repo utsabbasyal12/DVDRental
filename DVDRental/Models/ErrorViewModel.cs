a80389a915603edf510e67dfc5acdd095636adb6
Username <utsabbasyal12@gmail.com>
2022-04-10 07:29:47 +0545

    Add project files.

---
+++ DVDRental/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DVDRental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
