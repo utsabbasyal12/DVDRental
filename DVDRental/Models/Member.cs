97af335f3ce0e90b89772160da7968363fbe14ad
Mimona Karki <kmimo7na@gmail.com>
2022-04-29 11:50:26 +0545

    Added Rest of the controller

--- DVDRental/Models/Member.cs
+++ DVDRental/Models/Member.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DVDRental.Models
 {
+    [NotMapped]
     public class Member
     {
         [Key]
