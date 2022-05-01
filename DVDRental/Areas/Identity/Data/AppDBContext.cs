d3a9a2f37b35403c297cf1ffa836c0856d09d2e9
Username <utsabbasyal12@gmail.com>
2022-05-01 23:55:10 +0545

    Update DbContext File

--- DVDRental/Areas/Identity/Data/AppDBContext.cs
+++ DVDRental/Areas/Identity/Data/AppDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using DVDRental.Areas.Identity.Data;
+using DVDRental.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -8,17 +9,41 @@ namespace DVDRental.Areas.Identity.Data;
 
 public class AppDBContext : IdentityDbContext<DVDRentalUser>
 {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     public AppDBContext(DbContextOptions<AppDBContext> options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         : base(options)
     {
     }
+    public DbSet<Actor> Actors { get; set; }
+    public DbSet<Studio> Studios { get; set; }
+    public DbSet<CastMember> CastMembers { get; set; }
+    public DbSet<Producer> Producers { get; set; }
+    public DbSet<DVDCategory> DVDCategory { get; set; }
+    public DbSet<DVDTitle> DVDTitles { get; set; }
+    public DbSet<DVDCopy> DVDCopies { get; set; }
+    public DbSet<Loan> Loans { get; set; }
+    public DbSet<LoanType> LoanTypes { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<MembershipCategory> MembershipCategories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.Entity<CastMember>().HasKey(am => new
+        {
+            am.ActorId,
+            am.DVDNumber
+        });
+        builder.Entity<CastMember>()
+            .HasOne(a => a.Actor)
+            .WithMany(ab => ab.CastMembers)
+            .HasForeignKey(a => a.ActorId);
+
+        builder.Entity<CastMember>()
+            .HasOne(a => a.DVDTitle)
+            .WithMany(ab => ab.CastMembers)
+            .HasForeignKey(a => a.DVDNumber);
 
         builder.ApplyConfiguration(new ApplicationUserEntityCongiguration());
     }
