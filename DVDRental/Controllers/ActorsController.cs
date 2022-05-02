22aa134134f15cf2189f3fb35b12e1efc59376ca
Username <utsabbasyal12@gmail.com>
2022-05-02 11:57:20 +0545

    Added Controller and views

---
+++ DVDRental/Controllers/ActorsController.cs
@@ -0,0 +1,154 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DVDRental.Areas.Identity.Data;
+using DVDRental.Models;
+
+namespace DVDRental.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public ActorsController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Actors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Actors.ToListAsync());
+        }
+
+        // GET: Actors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await _context.Actors
+                .FirstOrDefaultAsync(m => m.ActorId == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return View(actor);
+        }
+
+        // GET: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Actors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ActorId,ActorSurname,ActorFirstName")] Actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(actor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(actor);
+        }
+
+        // GET: Actors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await _context.Actors.FindAsync(id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: Actors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ActorId,ActorSurname,ActorFirstName")] Actor actor)
+        {
+            if (id != actor.ActorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(actor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ActorExists(actor.ActorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(actor);
+        }
+
+        // GET: Actors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var actor = await _context.Actors
+                .FirstOrDefaultAsync(m => m.ActorId == id);
+            if (actor == null)
+            {
+                return NotFound();
+            }
+
+            return View(actor);
+        }
+
+        // POST: Actors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actor = await _context.Actors.FindAsync(id);
+            _context.Actors.Remove(actor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ActorExists(int id)
+        {
+            return _context.Actors.Any(e => e.ActorId == id);
+        }
+    }
+}
---
+++ DVDRental/Controllers/DVDTitlesController.cs
@@ -0,0 +1,182 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DVDRental.Areas.Identity.Data;
+using DVDRental.Models;
+
+namespace DVDRental.Controllers
+{
+    public class DVDTitlesController : Controller
+    {
+        private readonly AppDBContext _context;
+
+        public DVDTitlesController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DVDTitles
+        public async Task<IActionResult> Index()
+        {
+            var appDBContext = _context.DVDTitles.Include(d => d.DVDCategory).Include(d => d.Producers).Include(d => d.Studios);
+            return View(await appDBContext.ToListAsync());
+        }
+
+        // GET: DVDTitles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dVDTitle = await _context.DVDTitles
+                .Include(d => d.DVDCategory)
+                .Include(d => d.Producers)
+                .Include(d => d.Studios)
+                .FirstOrDefaultAsync(m => m.DVDNumber == id);
+            if (dVDTitle == null)
+            {
+                return NotFound();
+            }
+
+            return View(dVDTitle);
+        }
+
+        // GET: DVDTitles/Create
+        public async IActionResult Create()
+        {
+            var dvdDropdownData = await _context.GetNewDVDDropdownsValues();
+
+            ViewBag.Studios = new SelectList(dvdDropdownData.Studios, "StudioId", "StudioName");
+            ViewBag.Producers= new SelectList(dvdDropdownData.Producers, "ProducerNumber", "ProducerName");
+            ViewBag.Actors = new SelectList(dvdDropdownData.Actors, "ActorId", "FirstName");
+
+            return View();
+/*
+            ViewData["CategoryNumber"] = new SelectList(_context.DVDCategory, "CategoryNumber", "CategoryNumber");
+            ViewData["ProducerNumber"] = new SelectList(_context.Producers, "ProducerNumber", "ProducerName");
+            ViewData["StudioId"] = new SelectList(_context.Studios, "StudioId", "StudioName");
+
+            return View();*/
+        }
+
+        // POST: DVDTitles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DVDNumber,Title,DateRelease,StandardCharge,PenaltyCharge,StudioId,ProducerNumber,CategoryNumber")] DVDTitle dVDTitle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dVDTitle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryNumber"] = new SelectList(_context.DVDCategory, "CategoryNumber", "CategoryNumber", dVDTitle.CategoryNumber);
+            ViewData["ProducerNumber"] = new SelectList(_context.Producers, "ProducerNumber", "ProducerName", dVDTitle.ProducerNumber);
+            ViewData["StudioId"] = new SelectList(_context.Studios, "StudioId", "StudioName", dVDTitle.StudioId);
+            return View(dVDTitle);
+        }
+
+        // GET: DVDTitles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dVDTitle = await _context.DVDTitles.FindAsync(id);
+            if (dVDTitle == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryNumber"] = new SelectList(_context.DVDCategory, "CategoryNumber", "CategoryNumber", dVDTitle.CategoryNumber);
+            ViewData["ProducerNumber"] = new SelectList(_context.Producers, "ProducerNumber", "ProducerName", dVDTitle.ProducerNumber);
+            ViewData["StudioId"] = new SelectList(_context.Studios, "StudioId", "StudioName", dVDTitle.StudioId);
+            return View(dVDTitle);
+        }
+
+        // POST: DVDTitles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DVDNumber,Title,DateRelease,StandardCharge,PenaltyCharge,StudioId,ProducerNumber,CategoryNumber")] DVDTitle dVDTitle)
+        {
+            if (id != dVDTitle.DVDNumber)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dVDTitle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DVDTitleExists(dVDTitle.DVDNumber))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryNumber"] = new SelectList(_context.DVDCategory, "CategoryNumber", "CategoryNumber", dVDTitle.CategoryNumber);
+            ViewData["ProducerNumber"] = new SelectList(_context.Producers, "ProducerNumber", "ProducerName", dVDTitle.ProducerNumber);
+            ViewData["StudioId"] = new SelectList(_context.Studios, "StudioId", "StudioName", dVDTitle.StudioId);
+            return View(dVDTitle);
+        }
+
+        // GET: DVDTitles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dVDTitle = await _context.DVDTitles
+                .Include(d => d.DVDCategory)
+                .Include(d => d.Producers)
+                .Include(d => d.Studios)
+                .FirstOrDefaultAsync(m => m.DVDNumber == id);
+            if (dVDTitle == null)
+            {
+                return NotFound();
+            }
+
+            return View(dVDTitle);
+        }
+
+        // POST: DVDTitles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dVDTitle = await _context.DVDTitles.FindAsync(id);
+            _context.DVDTitles.Remove(dVDTitle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DVDTitleExists(int id)
+        {
+            return _context.DVDTitles.Any(e => e.DVDNumber == id);
+        }
+    }
+}
