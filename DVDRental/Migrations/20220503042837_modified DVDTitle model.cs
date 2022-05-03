8a65bdf29fd34fcd3818e38509aafd9173db8bef
Mimona Karki <kmimo7na@gmail.com>
2022-05-03 11:19:48 +0545

    not mapped removed from model DVDTitle

---
+++ DVDRental/Migrations/20220503042837_modified DVDTitle model.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DVDRental.Migrations
+{
+    public partial class modifiedDVDTitlemodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
