9679e70fb7d805ef7cf57b3dd455c1e0ed8dc493
Username <utsabbasyal12@gmail.com>
2022-05-03 13:28:02 +0545

    Services Error

--- DVDRental/Data/Services/ActorServices.cs
+++ DVDRental/Data/Services/ActorServices.cs
@@ -2,6 +2,16 @@
 {
     public class ActorServices: IActorServices
     {
-        
+        public Blog GetBlog(int blogId)
+        {
+            return appDbContext.Blogs
+                .Include(blog => blog.Creator)
+                .Include(blog => blog.Comments)
+                    .ThenInclude(comment => comment.Author)
+                .Include(blog => blog.Comments)
+                    .ThenInclude(comment => comment.Comments)
+                        .ThenInclude(reply => reply.Parent)
+                .FirstOrDefault(blog => blog.Id == blogId);
+        }
     }
 }
